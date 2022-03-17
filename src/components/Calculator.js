19e0a1aca4c2f9195fb5d98d1e970cc54bc49790
Tufahel <tufahel96@gmail.com>
2022-03-17 21:26:03 +0600

    refactor with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [calc, setCalc] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleCalculate = (event) => {
-    this.setState((state) => calculate(state, event.target.textContent));
-  }
+  const handleCalculate = (event) => {
+    setCalc((calc) => calculate(calc, event.target.textContent));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="grid-container">
-        <p className="item0">
-          {total}
-          {' '}
-          {operation}
-          {' '}
-          {next}
-        </p>
-        <button onClick={this.handleCalculate} type="button" className="item1">AC</button>
-        <button onClick={this.handleCalculate} type="button" className="item2">+/-</button>
-        <button onClick={this.handleCalculate} type="button" className="item3">%</button>
-        <button onClick={this.handleCalculate} type="button" className="item4">÷</button>
-        <button onClick={this.handleCalculate} type="button" className="item5">7</button>
-        <button onClick={this.handleCalculate} type="button" className="item6">8</button>
-        <button onClick={this.handleCalculate} type="button" className="item7">9</button>
-        <button onClick={this.handleCalculate} type="button" className="item8">x</button>
-        <button onClick={this.handleCalculate} type="button" className="item9">4</button>
-        <button onClick={this.handleCalculate} type="button" className="item10">5</button>
-        <button onClick={this.handleCalculate} type="button" className="item11">6</button>
-        <button onClick={this.handleCalculate} type="button" className="item12">-</button>
-        <button onClick={this.handleCalculate} type="button" className="item13">1</button>
-        <button onClick={this.handleCalculate} type="button" className="item14">2</button>
-        <button onClick={this.handleCalculate} type="button" className="item15">3</button>
-        <button onClick={this.handleCalculate} type="button" className="item16">+</button>
-        <button onClick={this.handleCalculate} type="button" className="item17">0</button>
-        <button onClick={this.handleCalculate} type="button" className="item18">.</button>
-        <button onClick={this.handleCalculate} type="button" className="item19">=</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="grid-container">
+      <p className="item0">
+        {calc.total}
+        {' '}
+        {calc.operation}
+        {' '}
+        {calc.next}
+      </p>
+      <button onClick={handleCalculate} type="button" className="item1">AC</button>
+      <button onClick={handleCalculate} type="button" className="item2">+/-</button>
+      <button onClick={handleCalculate} type="button" className="item3">%</button>
+      <button onClick={handleCalculate} type="button" className="item4">÷</button>
+      <button onClick={handleCalculate} type="button" className="item5">7</button>
+      <button onClick={handleCalculate} type="button" className="item6">8</button>
+      <button onClick={handleCalculate} type="button" className="item7">9</button>
+      <button onClick={handleCalculate} type="button" className="item8">x</button>
+      <button onClick={handleCalculate} type="button" className="item9">4</button>
+      <button onClick={handleCalculate} type="button" className="item10">5</button>
+      <button onClick={handleCalculate} type="button" className="item11">6</button>
+      <button onClick={handleCalculate} type="button" className="item12">-</button>
+      <button onClick={handleCalculate} type="button" className="item13">1</button>
+      <button onClick={handleCalculate} type="button" className="item14">2</button>
+      <button onClick={handleCalculate} type="button" className="item15">3</button>
+      <button onClick={handleCalculate} type="button" className="item16">+</button>
+      <button onClick={handleCalculate} type="button" className="item17">0</button>
+      <button onClick={handleCalculate} type="button" className="item18">.</button>
+      <button onClick={handleCalculate} type="button" className="item19">=</button>
+    </div>
+  );
+};
 
 export default Calculator;
